e059909608f303563b832ea278b143e6c6bdb329
Aman Madan <madan.aman@gmail.com>
2018-03-23 18:29:01 +0530

    commit

--- fractalTree.py
+++ fractalTree.py
@@ -2,18 +2,24 @@ import turtle
 
 def tree(branchLen,t):
     if branchLen>5:
+        # t.width(branchLen-10)
         t.forward(branchLen)
         t.right(20)
         tree(branchLen-15,t)
+        # t.width(branchLen-10)
         t.left(40)
         tree(branchLen-15,t)
+        # t.width(branchLen-10)
         t.right(20)
         t.backward(branchLen)
 def main():
     t = turtle.Turtle()
     myWin = turtle.Screen()
     t.left(90)
-    t.color("green")
+    # t.up()
+    # t.backward(100)
+    # t.down()
+    t.color("brown")
     tree(75,t)
     myWin.exitonclick()
 
