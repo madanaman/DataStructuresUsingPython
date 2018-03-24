caefa053a79704957cc63dcff74ee77efc673331
Aman Madan <madan.aman@gmail.com>
2018-03-24 06:12:11 +0530

    Fractal Changes

--- fractalTree.py
+++ fractalTree.py
@@ -2,15 +2,19 @@ import turtle
 
 def tree(branchLen,t):
     if branchLen>5:
-        # t.width(branchLen-10)
+        #t.width(branchLen)
         t.forward(branchLen)
         t.right(20)
         tree(branchLen-15,t)
-        # t.width(branchLen-10)
         t.left(40)
         tree(branchLen-15,t)
         # t.width(branchLen-10)
         t.right(20)
+        # To ensure that leaf node stays green and rest of the tree stays brown
+        if branchLen!=15:
+            t.color('brown')
+        else:
+            t.color('green')
         t.backward(branchLen)
 def main():
     t = turtle.Turtle()
